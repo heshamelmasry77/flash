1ff2133049c0a6e797ad63eabb751ca9c6bcbec7
Hesham El Masry <heshamelmasry@outlook.com>
2018-04-16 10:18:42 +0200

    getting messages from the server

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
+
+let messages = [
+  {name: 'hesham', message: 'test 1'},
+  {name: 'dod', message: 'test2'},
+  {name: 'yasmine', message: 'test3'}
+];
+
+app.get('/messages', (req, res) => {
+  res.send(messages);
+});
+
 const server = app.listen(3008, () => {
   console.log('server is listening on port', server.address().port)
 });
