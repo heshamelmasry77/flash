a1af652c2981cda95e965af542a71319f77aa8fb
Hesham El Masry <heshamelmasry@outlook.com>
2018-04-16 20:07:58 +0200

    adding the front end for the chat bot

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser =  require('body-parser');
 const app = express();
 
 app.use(express.static(__dirname));
+// This let's body parser know that we expect JSON to be coming in with our HTTP request
+app.use(bodyParser.json());
 
 let messages = [
   {name: 'hesham', message: 'test 1'},
@@ -12,6 +15,16 @@ let messages = [
 app.get('/messages', (req, res) => {
   res.send(messages);
 });
+
+
+app.post('/messages', (req, res) => {
+  console.log(req.body);
+  // adding the new message to my array messages
+  messages.push(req.body);
+  res.sendStatus(200);
+});
+
+
 
 const server = app.listen(3008, () => {
   console.log('server is listening on port', server.address().port)
